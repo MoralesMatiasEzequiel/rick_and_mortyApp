3be932299b1899e921b73e115f99431426375cbd
Matías Morales <programacionfrida@gmail.com>
2024-04-12 16:42:13 -0300

    Create DB

---
+++ BACK/src/db.js
@@ -0,0 +1,9 @@
+require('dotenv').config();
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`, { logging: false, native: false })
+
+
+
+module.exports = {sequelize};
