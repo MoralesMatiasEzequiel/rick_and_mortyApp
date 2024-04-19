139e79ca342f98ac511aece8556be5086c6e1f23
Matías Morales <programacionfrida@gmail.com>
2024-04-19 13:39:18 -0300

    Install Cors

--- BACK/src/app.js
+++ BACK/src/app.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
 const routes = require('./routes/index');
 
 const server = express();
 
+server.use(cors());
 server.use(express.json());
 server.use(morgan('dev'));
+server.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*'); 
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
 
 server.use('/', routes);
 
